c4602b3aaa1c2c4aa0a1adfe739e8a53f93f38c3
LyzaTheLegendary <107208772+LyzaTheLegendary@users.noreply.github.com>
2023-10-11 10:03:44 +0200

    fix - added client csproj to sln

--- Common/Network/NetListener/TcpListener.cs
+++ Common/Network/NetListener/TcpListener.cs
@@ -1,38 +1,12 @@
-﻿using Common.Network.Clients;
-using Common.Threading;
-using System.Net;
-using System.Net.Sockets;
-using TcpClient = Common.Network.Clients.TcpClient;
-
-namespace Common.Network.NetListener
-{
+﻿namespace Common.Network.NetListener {
     public class TcpListener : IListener {
-        Socket listenerSock;
-        CancellationTokenSource cancelToken = new();
-        TaskPool pool = new(2);
-        public TcpListener(string address, int port)
-        {
-            IPEndPoint ep = new(IPAddress.Parse(address), port);
-            listenerSock = new(ep.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            listenerSock.Bind(ep);
-            listenerSock.Listen();
+        TaskPool
+        public void Start(Action<IClient, Header, byte[]> onRecv) {
+            throw new NotImplementedException();
         }
-        public void Start(Action<IClient> onConnect) {
-            Task.Run(() =>
-            {
-            while (true) // we don't need a is alive variable as we cancel it using a cancel token
-            {
-                Socket remoteSock = listenerSock.Accept();
-                pool.PendTask(() => { onConnect(new TcpClient(remoteSock)); });
 
-                }
-            },cancelToken.Token);
-        }
-        /// <summary>
-        /// Closes the listener, However the socket is still active, Meaning you can restart it by calling the start method again!
-        /// </summary>
         public void Stop() {
-            cancelToken.Cancel();
+            throw new NotImplementedException();
         }
     }
 }
